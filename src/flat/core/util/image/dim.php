293bd1f002366dc74a1baf09ea16956c8d14aee0
Doug Bird <doug@katmore.com>
2016-11-04 00:10:39 -0700

    image dim
    Signed-off-by: Doug Bird <doug@katmore.com>

--- src/flat/core/util/image/dim.php
+++ src/flat/core/util/image/dim.php
@@ -1,36 +1,36 @@
 <?php
 /**
- * \flat\core\util\image\dim class 
- *
- * PHP version >=5.6
- * 
- * Copyright (c) 2012-2015 Doug Bird. 
- *    All Rights Reserved. 
- * 
- * BY ACCESSING THE CONTENTS OF THIS SOURCE FILE IN ANY WAY YOU AGREE TO BE 
- * BOUND BY THE PROVISIONS OF THE "SOURCE FILE ACCESS AGREEMENT", A COPY OF 
- * WHICH CAN IS FOUND IN THE FILE 'LICENSE.txt'.
- * 
- * NO LICENSE, EXPRESS OR IMPLIED, BY THE COPYRIGHT OWNERS
- * OR OTHERWISE, IS GRANTED TO ANY INTELLECTUAL PROPERTY IN THIS SOURCE FILE.
- *
- * ALL WORKS HEREIN ARE CONSIDERED TO BE TRADE SECRETS, AND AS SUCH ARE AFFORDED 
- * ALL CRIMINAL AND CIVIL PROTECTIONS AS APPLICABLE.
- * 
- * @license see /flat/LICENSE.txt
- */
+ * \flat\core\util\image\dim class
+*
+* PHP version >=5.6
+*
+* Copyright (c) 2012-2015 Doug Bird.
+*    All Rights Reserved.
+*
+* BY ACCESSING THE CONTENTS OF THIS SOURCE FILE IN ANY WAY YOU AGREE TO BE
+* BOUND BY THE PROVISIONS OF THE "SOURCE FILE ACCESS AGREEMENT", A COPY OF
+* WHICH CAN IS FOUND IN THE FILE 'LICENSE.txt'.
+*
+* NO LICENSE, EXPRESS OR IMPLIED, BY THE COPYRIGHT OWNERS
+* OR OTHERWISE, IS GRANTED TO ANY INTELLECTUAL PROPERTY IN THIS SOURCE FILE.
+*
+* ALL WORKS HEREIN ARE CONSIDERED TO BE TRADE SECRETS, AND AS SUCH ARE AFFORDED
+* ALL CRIMINAL AND CIVIL PROTECTIONS AS APPLICABLE.
+*
+* @license see /flat/LICENSE.txt
+*/
 namespace flat\core\util\image;
 /**
  * image dimension data
- * 
+ *
  * @package    flat\core\util\image
  * @author     D. Bird <retran@gmail.com>
  * @copyright  Copyright (c) 2012-2015 Doug Bird. All Rights Reserved.
  * @version    0.1.0-alpha
- * 
- * @todo impelment alpha, dpi properties 
+ *
+ * @todo impelment alpha, dpi properties
  */
-class dim extends \flat\data 
+class dim extends \flat\data
 implements \flat\data\ready
 {
    public $height;
@@ -41,52 +41,52 @@ implements \flat\data\ready
    public $w;
    public $alpha;
    public $dpi;
-   
+    
    /**
     * calculates dimension for scaling up an image given the minumum dimension constraint
-    * 
+    *
     * @return \flat\core\util\image\dim
-    * 
+    *
     * @param \flat\core\util\image\dim $image original image dimensions
     * @param \flat\core\util\image\dim $min minimum dimension constraint
     */
    public static function scale_up(dim $image, dim $min) {
        
-       if (
-         ($image->w < $min->w) || //width undersized 
-         ($image->h < $min->h) //height undersized
-       ) {
-             return new dim(
-               array(
-                  'width' => round( max($min->w, $image->ratio() * $min->h) ),
-                  'height' => round( max($min->h, $min->w / $image->ratio()) )
-               )
-             );
-       }
-       return $image;
-   }   
+      if (
+            ($image->w < $min->w) || //width undersized
+            ($image->h < $min->h) //height undersized
+            ) {
+               return new dim(
+                     array(
+                        'width' => round( max($min->w, $image->ratio() * $min->h) ),
+                        'height' => round( max($min->h, $min->w / $image->ratio()) )
+                     )
+                     );
+            }
+            return $image;
+   }
    /**
     * calculates dimension for scaling down an image given the minumum dimension constraint
-    * 
+    *
     * @return \flat\core\util\image\dim | bool
-    * 
+    *
     * @param \flat\core\util\image\dim $image original image dimensions
     * @param \flat\core\util\image\dim $max maximum dimension constraint
     */
    public static function scale_down(dim $image, dim $max){
        
-       if (
-         ($image->w > $max->w) || //width oversized 
-         ($image->h > $max->h) //height oversized
-       ) {
-             return new dim (array(
-               'width' => round( min($max->w, $image->ratio() * $max->h) ), 
-               'height' => round( min($max->h, $max->w / $image->ratio()) )
-             ));
-       }
-       return $image;
-   }   
-   
+      if (
+            ($image->w > $max->w) || //width oversized
+            ($image->h > $max->h) //height oversized
+            ) {
+               return new dim (array(
+                  'width' => round( min($max->w, $image->ratio() * $max->h) ),
+                  'height' => round( min($max->h, $max->w / $image->ratio()) )
+               ));
+            }
+            return $image;
+   }
+    
    public static function scale_dim(dim $image, dim $min, dim $max) {
       /*
        * scale to max
@@ -100,56 +100,56 @@ implements \flat\data\ready
    }
    /**
     * calculates dimension for scaling an image given the minimum and maximum dimension constraints
-    * 
+    *
     * @return void
-    * 
+    *
     * @param dim $min minimum dimension constraint
     * @param dim $max maximum dimension constraint
     */
    public function scale(dim $min=NULL, dim $max=NULL) {
       if ($min && $max) {
          parent::__construct(
-            (array) self::scale_dim($this, $min, $max)
-         );
+               (array) self::scale_dim($this, $min, $max)
+               );
       } else
-      if ($min) {
-         parent::__construct(
-            (array) self::scale_up_dim($this,$min)
-         );
-      } else
-      if ($max) {
-         parent::__construct(
-            (array) self::scale_down_dim($this, $max)
-         );
-      }
+         if ($min) {
+            parent::__construct(
+                  (array) self::scale_up($this,$min)
+                  );
+         } else
+            if ($max) {
+               parent::__construct(
+                     (array) self::scale_down($this, $max)
+                     );
+            }
    }
-   
+    
    /**
     * calculates ratio of width to height
-    * 
+    *
     * @return float
     */
    public function ratio() {
-      return (float) $this->w * 1.0 / $this->h;      
+      return (float) $this->w * 1.0 / $this->h;
    }
-   
+    
    /**
     * magic method __toString()
-    * 
+    *
     */
    public function __toString() {
       return (string) $this->width . "x". $this->height;
    }
-   
+    
    protected $imagick;
    /**
     * invoked once all data has been mapped
-    * 
+    *
     * @see \flat\data\transform part of the data transform interface
-    * 
+    *
     * @return void
-    * 
-    * @param array $data data (if any) that was already given and mapped 
+    *
+    * @param array $data data (if any) that was already given and mapped
     *    by /flat/data::__construct().
     */
    public function data_ready() {
@@ -161,7 +161,7 @@ implements \flat\data\ready
          $this->width = $imagic->getImageWidth();
       }
       /*
-       * if $width, $heigh properties empty try and derive 
+       * if $width, $heigh properties empty try and derive
        * from shortcut properties:
        *       $x,$w (for $width)
        *       $y,$h (for $height)
@@ -171,32 +171,32 @@ implements \flat\data\ready
          if (!empty($this->x)) {
             $this->width = $this->x;
          } else
-         if (!empty($this->w)) {
-            $this->width = $this->w;
-         }
+            if (!empty($this->w)) {
+               $this->width = $this->w;
+            }
       }
       if (empty($this->height)) {
          if (!empty($this->y)) {
             $this->height = $this->y;
          } else
-         if (!empty($this->h)) {
-            $this->height = $this->h;
-         }
+            if (!empty($this->h)) {
+               $this->height = $this->h;
+            }
       }
-      
+
       /*
        * sanity enforcement:
        *    height and width must be values that can be evaluated to integer
        */
       foreach (array('height','width') as $prop) {
          if (is_string($this->$prop) && !is_numeric($this->$prop)) throw new exception\bad_dim(
-            "$prop must be numeric"
-         );
+               "$prop must be numeric"
+               );
          if (empty($this->$prop)) $this->$prop = 0;
          if (!is_int($this->$prop)) $this->$prop = (int) sprintf("%d",$this->$prop);
          if ($this->$prop<0) throw new exception\bad_dim(
-            "$prop must evaluate to an integer 0 or greater"
-         );
+               "$prop must evaluate to an integer 0 or greater"
+               );
       }
 
       /*
