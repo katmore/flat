85ce0a0f4d6db1e50507e81dc5c596f1d59d5ea1
Doug Bird <doug@katmore.com>
2017-03-08 16:58:54 -0800

    corrected dependencies

--- src/flat/cloud/authorize.php
+++ src/flat/cloud/authorize.php
@@ -1,35 +1,35 @@
 <?php
 /**
- * class definition 
- *
- * PHP version >=7.1
- * 
- * Copyright (c) 2012-2017 Doug Bird. 
- *    All Rights Reserved. 
- * 
- * COPYRIGHT NOTICE:
- * The flat framework. https://github.com/katmore/flat
- * Copyright (c) 2012-2017  Doug Bird.
- * ALL RIGHTS RESERVED. THIS COPYRIGHT APPLIES TO THE ENTIRE CONTENTS OF THE WORKS HEREIN
- * UNLESS A DIFFERENT COPYRIGHT NOTICE IS EXPLICITLY PROVIDED WITH AN EXPLANATION OF WHERE
- * THAT DIFFERENT COPYRIGHT APPLIES. WHERE SUCH A DIFFERENT COPYRIGHT NOTICE IS PROVIDED
- * IT SHALL APPLY EXCLUSIVELY TO THE MATERIAL AS DETAILED WITHIN THE NOTICE.
- * 
- * The flat framework is copyrighted free software.
- * You can redistribute it and/or modify it under either the terms and conditions of the
- * "The MIT License (MIT)" (see the file MIT-LICENSE.txt); or the terms and conditions
- * of the "GPL v3 License" (see the file GPL-LICENSE.txt).
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * @license The MIT License (MIT) http://opensource.org/licenses/MIT
- * @license GNU General Public License, version 3 (GPL-3.0) http://opensource.org/licenses/GPL-3.0
- * @link https://github.com/katmore/flat
- * @author     D. Bird <retran@gmail.com>
- * @copyright  Copyright (c) 2012-2017 Doug Bird. All Rights Reserved.
- */
+ * class definition
+*
+* PHP version >=7.1
+*
+* Copyright (c) 2012-2017 Doug Bird.
+*    All Rights Reserved.
+*
+* COPYRIGHT NOTICE:
+* The flat framework. https://github.com/katmore/flat
+* Copyright (c) 2012-2017  Doug Bird.
+* ALL RIGHTS RESERVED. THIS COPYRIGHT APPLIES TO THE ENTIRE CONTENTS OF THE WORKS HEREIN
+* UNLESS A DIFFERENT COPYRIGHT NOTICE IS EXPLICITLY PROVIDED WITH AN EXPLANATION OF WHERE
+* THAT DIFFERENT COPYRIGHT APPLIES. WHERE SUCH A DIFFERENT COPYRIGHT NOTICE IS PROVIDED
+* IT SHALL APPLY EXCLUSIVELY TO THE MATERIAL AS DETAILED WITHIN THE NOTICE.
+*
+* The flat framework is copyrighted free software.
+* You can redistribute it and/or modify it under either the terms and conditions of the
+* "The MIT License (MIT)" (see the file MIT-LICENSE.txt); or the terms and conditions
+* of the "GPL v3 License" (see the file GPL-LICENSE.txt).
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* @license The MIT License (MIT) http://opensource.org/licenses/MIT
+* @license GNU General Public License, version 3 (GPL-3.0) http://opensource.org/licenses/GPL-3.0
+* @link https://github.com/katmore/flat
+* @author     D. Bird <retran@gmail.com>
+* @copyright  Copyright (c) 2012-2017 Doug Bird. All Rights Reserved.
+*/
 namespace flat\cloud;
 
 use \net\authorize\api\contract\v1 as AnetAPI;
@@ -68,25 +68,25 @@ use \net\authorize\util\LogFactory;
  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
- 
-   const CONFIG_NS_PREFIX= 'app/cloud/activepitch/authorize';
-   
-   protected function _get_transaction_key() {
-      return "my-transaction-key";
-   }
-
-   protected function _get_login_id() {
-      return "my-login-id";
-   }
-   
-   protected function _get_end_point() {
-      return \net\authorize\api\constants\ANetEnvironment::SANDBOX;
-   }
-   
-   protected function _get_log_file() {
-      return "";
-   }
- 
+ &nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;const CONFIG_NS_PREFIX= 'app/cloud/activepitch/authorize';
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;protected function _get_transaction_key() {
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return "my-transaction-key";
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+ &nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;protected function _get_login_id() {
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return "my-login-id";
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;protected function _get_end_point() {
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return \net\authorize\api\constants\ANetEnvironment::SANDBOX;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;protected function _get_log_file() {
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return "";
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
  &nbsp;&nbsp;&nbsp;};
  &nbsp;&nbsp;&nbsp;//
  &nbsp;&nbsp;&nbsp;// ---END CODE EXAMPLE---
@@ -94,47 +94,47 @@ use \net\authorize\util\LogFactory;
  </code></pre>
  */
 abstract class authorize {
-   
+    
    /**
     * Specifies the merchant's transaction key.
-    * 
+    *
     * @return string
     */
    abstract protected function _get_transaction_key();
-   
+    
    /**
     * Specifies the merchant's API login ID.
     *
     * @return string
     */
    abstract protected function _get_login_id();
-   
+    
    /**
     * Specifies the API end point URL
     * @return string
     */
    abstract protected function _get_end_point();
-   
+    
    /**
     * Specifies the API end point URL
     * @return string
     */
    abstract protected function _get_log_file();
-    
+
    /**
     * prepares merchant authentication object
     *
     * @return \net\authorize\api\contract\v1\MerchantAuthenticationType
     */
    protected function _load_MerchantAuthentication() {
-   
+       
       $merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
       $merchantAuthentication->setName($this->_get_login_id());
       $merchantAuthentication->setTransactionKey($this->_get_transaction_key());
-   
+       
       return $merchantAuthentication;
    }
-    
+
    /**
     * convenience wrapper to invoke a controller::executeWithApiResponse()
     *    and throw a response_error if the result code is not 'Ok'.
@@ -147,28 +147,28 @@ abstract class authorize {
     *
     */
    protected function _executeWithApiResponse(ApiOperationBase $controller) {
-   
+       
       if (!defined('AUTHORIZENET_LOG_FILE')) {
          define("AUTHORIZENET_LOG_FILE", '/phplog');
       }
-   
+       
       $logger = LogFactory::getLog(get_called_class());
-   
+       
       try {
          $response = $controller->executeWithApiResponse( $this->_get_end_point() );
       } catch (\Exception $e) {
          throw new \Exception(print_r($e,true));
       }
-   
+       
       if (($response != null) && ($response->getMessages()->getResultCode() == "Ok") )
       {
          return $response;
       }
-   
-   
+       
+       
       throw new response_error($response);
    }
-   
+    
 }
 
 
